232de4a6e9941d1366fcfe78e242beaa4ba95d63
Brendon Roberto <Brendon.Roberto@onbase.com>
2015-11-12 17:37:24 -0500

    Add user model, update server.js

---
+++ src/models/user.js
@@ -0,0 +1,52 @@
+module.exports = function(app) {
+    var Promise   = require('bluebird');
+    var bookshelf = app.get('bookshelf');
+    var bcrypt    = app.get('bcrypt');
+    var _         = app.get('underscore');
+    var debug     = app.get('debug');
+
+    var User = bookshelf.Model.extend({
+        tableName: 'users',
+        initialize: function() {
+            this.on('creating', this.onCreating, this);
+            this.on('saving', this.onSaving, this);
+        },
+        format: function(attrs) {
+            return _.omit(attrs, 'password');
+        },
+        onCreating: function() {
+            var email = this.get('email').toLowerCase().trim();
+            this.set('email', email);
+            this.hashPassword();
+            this.set('created_at', new Date());
+        },
+        onSaving: function() {
+            this.set('updated_at', new Date());
+        },
+        hashPassword: function() {
+            var password = this.get('password');
+            var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+            this.set('password_hash', hash);
+        },
+        toSecureJSON: function() {
+            return _.omit(this.toJSON(), ['password', 'password_hash']);
+        }
+    }, {
+        login: Promise.method(function(email, password) {
+            if (!email || !password) throw new Error('Email and password are both required');
+
+            return new User({ email: email.toLowerCase().trim() }).fetch({ required: true }).then(function(user) {
+                debug("User logging in: %j", user);
+                return Promise.promisify(bcrypt.compare)(password, user.get('password_hash'));
+            }).then(function(result) {
+                return (result) ? user : undefined;
+            });
+        })
+    });
+
+    if (!bookshelf.model('User')) {
+        bookshelf.model('User', User);
+    }
+
+    return User;
+};
---
+++ src/server.js
@@ -0,0 +1,60 @@
+var app = function(options) {
+    // Require dependencies
+    var express        = require('express'),
+        _              = require('lodash'),
+        passport       = require('passport'),
+        path           = require('path'),
+        bodyParser     = require('body-parser'),
+        debug          = require('debug')('development'),
+        cookieParser   = require('cookie-parser'),
+        morgan         = require('morgan'),
+        expressSession = require('express-session'),
+        bcrypt         = require('bcrypt'),
+        knex;
+
+    var app = express();
+
+    // Set libraries
+    app.set('underscore', _);
+    app.set('debug', debug);
+    app.set('bcrypt', bcrypt);
+
+    // Initialize DB
+    var config = require('knexfile')[process.env.CONFIGURATION_ENV] || {
+        client: 'sqlite3',
+        connection: {
+        filename: './dev.sqlite3'
+        }
+    };
+
+    knex = require('knex')(config);
+
+    var bookshelf = require('bookshelf')(knex);
+    bookshelf.plugin('registry');
+    app.set('bookshelf', bookshelf);
+
+    // Set up middleware
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    if (!options || (options && !options.squelch)) {
+        app.use(morgan('dev'));
+    }
+    app.use(expressSession({
+        secret: process.env.EXPRESS_SECRET_KEY,
+        resave: true,
+        saveUninitialized: true
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.set('passport', passport);
+
+    require('passport-init')(passport, require('models/user')(app));
+
+    // Require routes
+    require('routes')(app, express);
+
+
+    return app;
+};
+
+module.exports = app;
