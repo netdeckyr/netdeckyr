2450859e8e028b610ad82f380efd687b182e592b
Brendon Roberto <Brendon.Roberto@onbase.com>
2016-06-24 14:16:57 -0400

    Use named-routes in view helpers

--- src/views/base_view.js
+++ src/views/base_view.js
@@ -1,23 +1,29 @@
 // jshint esversion: 6
 
 var base_view = function(app) {
-    const _      = app.get('underscore');
+    const _      = app.locals.underscore;
     const extend = require('extensive');
 
-    var BaseView = extend(function(response, template) {
+    var BaseView = extend(function(response, template, router) {
         this.response = response;
         this.template = template;
+        this.router = router;
     }, {
         render: function(vars) {
+            vars.helpers = this.helpers;
             return this.response.render(this.template, vars);
         },
         helpers: {
             pageLink: function(name) {
-                return `page/${name}`;
+                return `/page/${name}`;
             },
-            
-            linkTo: function(name) {
-                return `/${name}`;
+
+            linkToRoute: function(router, routePath, variables) {
+                if (app.locals.routers && app.locals.routers[router]) {
+                    return `/${router + app.locals.routers[router].namedRoutes.build(routePath, variables)}`;
+                } else {
+                    return "/";
+                }
             }
         }
     }, {
